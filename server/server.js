6308af6449a717b33d5f5c33e6fa457e1b43db6c
Jonathan Slack <slack6000@gmail.com>
2022-06-15 20:54:38 -0500

    third save

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
+require('dotenv').config();
 const express = require("express");
-
 const path = require("path");
-
+const cors = require("cors")
 const app = express();
+const { SERVER_PORT } = process.env
+
+app.use(express.json())
+app.use(cors())
 
 
+//maybe I will need
+// app.post('/seed', seed)
 
-
-
-
+//appointments
+app.post('/appointments', createAppt)
+app.get('/appointments', getAppt)
+app.put('appointments', editAppt)
+app.delete('appointments', deleteAppt)
 
 
 
